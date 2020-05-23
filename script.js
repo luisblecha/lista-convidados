eb44f2ac05b412d3f78acb4549765c86096c6d0d
Luis Blecha <lh.blecha@hotmail.com>
2020-05-23 15:56:05 -0400

    atualizando projeto bootstrap

--- script.js
+++ script.js
@@ -19,9 +19,11 @@ function atualizarLista() {
     // adiciona os elementos
     for(i = 0; i < convidados.length; i++) { // i = 0 1 2
         lista.innerHTML += `
-        <div> 
-            <span>${convidados[i].nome}</span> 
-        </div>`
+        <tr> 
+            <td>${convidados[i].nome}</td> 
+            <td>${convidados[i].idade}</td> 
+            <td>${convidados[i].email}</td> 
+        </tr>`
     }
 
 }
